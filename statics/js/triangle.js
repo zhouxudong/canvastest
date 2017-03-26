420116c893ea77ce2e58412d0e66a4a20c3111ca
zhouxudong <z_zxdong@163.com>
2017-03-26 19:48:05 +0800

    判断三角形 是正面还是反面

--- statics/js/triangle.js
+++ statics/js/triangle.js
@@ -4,7 +4,7 @@ function Triangle(a, b, c, color) {
     this.pointC = c;
     this.color = utils.parseColor(color || "#ff0000");
     this.lineWidth = 1;
-    this.alpha = 0.5;
+    this.alpha = 1;
 }
 Triangle.prototype.draw = function (cxt) {
     cxt.save();
@@ -20,4 +20,13 @@ Triangle.prototype.draw = function (cxt) {
         cxt.stroke();
     }
     cxt.restore();
+}
+
+//计算三角形两条边的长度，然后用乘法和比较，用到 三角形相对于canvas的法线向量
+Triangle.prototype.isBackface = function () {
+    var cax = this.pointC.getScreenX() - this.pointA.getScreenX(),
+        cay = this.pointC.getScreenY() - this.pointA.getScreenY(),
+        bcx = this.pointB.getScreenX() - this.pointC.getScreenX(),
+        bcy = this.pointB.getScreenY() - this.pointC.getScreenY();
+    return (cax * bcy > cay * bcx);
 }
